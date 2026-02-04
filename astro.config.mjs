7a31258fd339cda10f7734a2da6d248046e03c6a
wllyvx <willyfr88@gmail.com>
2026-02-04 09:46:38 +0700

    implementing shadcn UI to website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
 
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  integrations: [react()],
   vite: {
       plugins: [tailwindcss()],
   },
