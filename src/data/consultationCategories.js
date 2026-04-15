bb5c3f06841a067e0fadd771f0b8f0f09d437aaf
wllyvx <willyfr88@gmail.com>
2026-04-15 14:38:46 +0700

    new feature: pojok konsultasi

---
+++ src/data/consultationCategories.js
@@ -0,0 +1,7 @@
+export const consultationCategories = [
+  { value: 'zakat', label: 'Zakat' },
+  { value: 'donasi', label: 'Donasi' },
+  { value: 'keuangan', label: 'Keuangan' },
+  { value: 'ibadah', label: 'Ibadah' },
+  { value: 'umum', label: 'Umum' }
+];
